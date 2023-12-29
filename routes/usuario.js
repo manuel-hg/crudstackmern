244a7fca187c441308e1b335896a5d367debfdf6
Juan Manuel <jhernandez@telat-group.com>
2023-12-29 15:51:57 -0600

    update fix second

--- routes/usuario.js
+++ routes/usuario.js
@@ -11,4 +11,98 @@ const eschemausuario = new eschema({
     idUsuario: String
 })
 
-const ModeloUsuario = mongoose.model();
+const ModeloUsuario = mongoose.model('usuarios', eschemausuario);
+module.exports = router
+
+//ruta de prueba
+/*router.get('/ejemplo', (req, res) => {
+    res.end('saludo carga desde ruta test');
+})*/
+
+router.post('/agregar', async (req, res) => {
+    try{
+        const nuevoUsuario = await new ModeloUsuario({
+            nombre: req.body.nombre,
+            email: req.body.email,
+            telefono: req.body.telefono,
+            idUsuario: req.body.id
+        })
+        nuevoUsuario.save();   
+
+        res.json({
+            status: 200,
+            response: 'Usuario guardado correctamente'
+        })
+    } catch(error){
+        res.json({
+            status: 404,
+            response: error
+        })        
+    }
+});
+
+router.post('/obtener', async (req, res) => {
+    try {
+        const grupos = await ModeloUsuario.find().exec()
+        res.json({
+            status: 200,
+            response: grupos
+        })   
+    } catch (error) {
+        res.json({
+            status: 404,
+            response: error
+        })   
+    }
+})
+
+router.post('/obtenerUsuario', async (req, res) => {
+    try {
+        const grupos = await ModeloUsuario.findOne({idUsuario: req.body.idUsuario}).exec()
+        res.json({
+            status: 200,
+            response: grupos
+        })   
+    } catch (error) {
+        res.json({
+            status: 404,
+            response: error
+        })   
+    }
+})
+
+router.post('/editar', async (req, res) => {
+    try {
+        const find = await ModeloUsuario.findOneAndUpdate({idUsuario: req.body.id}, {
+            nombre: req.body.nombre,
+            email: req.body.email,
+            telefono: req.body.telefono
+        }).exec();
+        res.json({
+            status: 200,
+            response: 'Se ha actualizado correctamente'
+        })   
+    } catch (error) {
+        res.json({
+            status: 404,
+            response: error
+        })   
+    }
+})
+
+router.post('/eliminar', async (req, res) => {
+    try {
+        const grupos = await ModeloUsuario.findOneAndDelete({idUsuario: req.body.id}).exec()
+        res.json({
+            status: 200,
+            response: 'Se ha eliminado correctamente'
+        })  
+    } catch (error) {
+        res.json({
+            status: 404,
+            response: error
+        })   
+    }
+})
+
+
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express()
+app.use(express.json());
+app.use(cors());
 
 //importar conexion con mongodb
 const dbfile = require('./conexion');
+
+//importar archivo de rutas
+const rutausuario = require("./routes/usuario");
+app.use('/api/usuario', rutausuario);
 
 app.get('/', (req, res) => {
     res.end('Bienvenidos al servidor backend Node.js');
